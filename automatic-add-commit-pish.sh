d6180e0119541ecd27ee1b86fb6eba49d5e20f45
Prasadrasal2002 <rasalprasad2002@gmail.com>
2024-12-02 06:27:24 +0000

    file is ready for commit

--- automatic-add-commit-pish.sh
+++ automatic-add-commit-pish.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 dir_path=/home/devops/project_file/git-dir
 
+echo "stage status................... "
 if [ ! -d $path ]
-then
-	echo "directory does not exist"
+	then
+		echo "directory does not exist"
 fi
 file_path=$dir_path/testing-file
-
-if [ -f $file_path ]
-then
-	git checkout -b  test-branch
-	touch $file_path
-fi
-
-if [ $?=0 ]
-then
-	echo "file is craeted successfully"
-else
-	echo "file not exist"
-fi
-if [ -f $file_path ]
-then
-	echo "untrack status................. "
-	git status
 	
-	git add .
-	echo "stage status................... "
-	git status
-	
-	git commit -m "file is ready for commit"
-	echo "tracked status................. "
-	git status
-
-	git config -l
-
-	git push new-origin 
+if [ -f $file_path ]
+	then
+		git checkout -b  test-branch
+		touch $file_path
 fi
-
-
-
-
-
-
+	
+	if [ $?=0 ]
+		then
+			echo "untrack status................. "
+			git status
+				
+			git add .
+			echo "stage status................... "
+			git status
+				
+	if [ $?=0 ]
+		then
+			git commit -m "file is ready for commit"
+			echo "tracked status................. "
+			git status
+			
+	if [ $?=0 ]
+		then
+			git push new-origin test-branch
+	fi
+	fi
+			
+	fi
+	
+	
+	
+	
+	
+	
 
 
