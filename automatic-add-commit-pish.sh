e4032f9b2c67b035429d1847fbb6cb6fb38795d0
Prasadrasal2002 <rasalprasad2002@gmail.com>
2024-12-02 05:11:48 +0000

    file is ready for commit

---
+++ automatic-add-commit-pish.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+dir_path=/home/devops/project_file/git-dir
+
+if [ ! -d $path ]
+then
+	echo "directory does not exist"
+fi
+file_path=$dir_path/testing-file
+
+if [ -f $file_path ]
+then
+	touch $file_path
+fi
+
+if [ $?=0 ]
+then
+	echo "file is craeted successfully"
+else
+	echo "file not exist"
+fi
+if [ -f $file_path ]
+then
+	echo "add"
+	git add .
+	echo "stage status"
+	git status
+	git commit -m "file is ready for commit"
+	echo "tracked status"
+	git status
+fi
+
+
+
+
+
+
+
